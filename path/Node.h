27eeb80fd05feea092c7501487d4817ad436ef7b
geoff whitehead <g.whitehead@ncl.ac.uk>
2016-03-22 23:26:02 +0000

    nearly working

--- path/Node.h
+++ path/Node.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include "../_resources/irrlicht-1.8.3/include/vector3d.h"
+#include <string>
 
 using namespace irr;
 using namespace core;
@@ -23,17 +24,18 @@ public:
 	int h;
 	int g;
 	int f;
+	Node* parent;
 	std::vector<Edge> edges;
+	bool passable = true;
 
 	friend ostream &operator<<(ostream &output,
-		const Node *N)
+		const Node N)
 	{
-		output 
-			<< "Node 1:" 
-			<< " Edge 1: " << N->edges[0].to_node << " Cost " << N->edges[0].cost
-			<< " Edge 2: " << N->edges[1].to_node << " Cost " << N->edges[1].cost
-			<< " Edge 3: " << N->edges[2].to_node << " Cost " << N->edges[2].cost
-			<< endl;
+		output
+			<< "Node 1:"
+			<< " Edge 1: " << N.edges[0].to_node << " Cost " << N.edges[0].cost
+			<< " Edge 2: " << N.edges[1].to_node << " Cost " << N.edges[1].cost
+			<< " Edge 3: " << N.edges[2].to_node << " Cost " << N.edges[2].cost;
 		return output;
 	}
 };
--- path/main.cpp
+++ path/main.cpp
@@ -6,8 +6,14 @@
 #include <driverChoice.h>
 #include "ShaderCallback.h"
 #include <string>
+#include <map>
+#include <algorithm>
+#include <assert.h>
 
 #define NODE_SIZE 0.5
+#define EDGES 3
+#define PATH_FOUND 0
+#define NO_PATH 1
 
 using namespace irr;
 using namespace core;
@@ -17,6 +23,10 @@ using namespace gui;
 
 void createNodes();
 void run();
+void shortestPath(Node* origin, Node* target);
+void printPath(Node* origin, Node* target);
+int aStar(std::map<Node*, Node*>* open_list, std::map<Node*, Node*>* closed_list, Node* current, Node* target);
+
 std::vector<Node*> nodes;
 
 float func() {
@@ -26,9 +36,6 @@ float func() {
 int main() {
 	
 	run();
-
-
-
 
 	UseHighLevelShaders = true;
 	UseCgShaders = true;
@@ -194,11 +201,7 @@ int main() {
 void run() {
 
 	createNodes();
-
-
-
-	Node start = *nodes[1];
-	Node end = *nodes[10];
+	shortestPath(nodes[1], nodes[10]);
 }
 
 void createNodes() {
@@ -264,23 +267,32 @@ void createNodes() {
 	nodes.push_back(new Node(vector3df(-2 * func(), -1, (2 + func()))));
 	nodes.push_back(new Node(vector3df(-2 * func(), -1, -(2 + func()))));
 
-
+	// for all nodes
 	for (int node_from = 0; node_from < nodes.size(); node_from++){
+		// for all nodes
 		for (int node_to = 0; node_to < nodes.size(); node_to++){
+			// if the nodes are not the same object
 			if (node_from != node_to) {
+				// calculate the distance between the two nodes
 				float cost = nodes[node_to]->pos.getDistanceFrom(nodes[node_from]->pos);
+				// if the nodes doesnt have 3 edges yet - add the new edge
 				if (nodes[node_from]->edges.size() < 3) {
 					nodes[node_from]->edges.push_back(Edge{ nodes[node_to], cost });
 				}
+				// else - continue to work out where to place the new edge
 				else {
+					// default the max index and cost to the first node in the edge array
 					int max_index = 0;
 					float max_cost = nodes[node_from]->edges[0].cost;
+					// for all edges of the node excluding the first  (this has already been considered as the default)
 					for (int edge = 1; edge < nodes[node_from]->edges.size(); edge++){
+						// if the edge cost is greater than the maximum; record the index and max cost
 						if (nodes[node_from]->edges[edge].cost > max_cost) {
 							max_index = edge;
 							max_cost = nodes[node_from]->edges[edge].cost;
 						}
 					}
+					// if the cost of the new edge is LESS than the cost of the most expensive edge; overwrite the most expensive edge in place of the current one.
 					if (cost < max_cost) {
 						nodes[node_from]->edges[max_index].to_node = nodes[node_to];
 						nodes[node_from]->edges[max_index].cost = cost;
@@ -292,7 +304,80 @@ void createNodes() {
 	}
 
 	for (int i = 0; i < nodes.size(); i++){
-		cout << nodes[i];
+		cout << "Node " << i << " {" << nodes[i] << "} {" << *nodes[i] << "}" << endl;
 	}
+}
 
+void shortestPath (Node* origin, Node* target) {
+	std::map<Node*, Node*> open_list;
+	std::map<Node*, Node*> closed_list;
+
+	Node* current = origin;
+	current->g = 0;
+
+	open_list.insert(std::pair<Node*, Node*>(current, current));
+	
+	if (aStar(&open_list, &closed_list, current, target) == PATH_FOUND)
+		printPath(origin, target);
+	else 
+		cout << "NO PATH" << endl;
+	
+}
+
+int aStar(std::map<Node*, Node*>* open_list, std::map<Node*, Node*>* closed_list, Node* current, Node* target) {
+	
+	Node* lowest_score = nullptr;
+
+	for (int i = 0; i < EDGES; i++)
+	{
+		Node* to_node = current->edges[i].to_node;
+
+		// add all edges into open list
+		if (closed_list->count(to_node) == 0 && to_node->passable == true) {
+			if (open_list->count(to_node) == 0) {
+				open_list->insert(std::pair<Node*, Node*>(to_node, current));
+				// fgh
+				to_node->h = to_node->pos.getDistanceFrom(target->pos);
+				to_node->g = current->g + current->edges[i].cost;
+				to_node->f = to_node->h + to_node->g;
+			}
+			else {
+				float temp_g_score = current->g + current->edges[i].cost;
+				if (temp_g_score < to_node->g) {
+					(*open_list)[to_node] = current;
+					to_node->h = to_node->pos.getDistanceFrom(target->pos);
+					to_node->g = current->g + current->edges[i].cost;
+					to_node->f = to_node->h + to_node->g;
+				}
+			}
+		}else 
+			cout << "BACKTRACK" << endl;
+		// find the lowest fScore
+	}
+	for (std::map<Node*, Node*>::iterator it = (*open_list).begin(); it != (*open_list).end(); ++it) {
+		if (lowest_score == nullptr)
+			lowest_score = (*open_list).begin()->first;
+		else {
+			if (it->first->f == lowest_score->f)
+				cout << "TIEBREAK NEEDED" << endl;
+			else
+				if (it->first->f < lowest_score->f)
+					lowest_score = it->first;
+		}
+	}
+	// remove the evaluated node from the open list and insert into closed list
+	std::map<Node*, Node*>::iterator it;
+	it = (*open_list).find(current);
+	if (it == (*open_list).end()) exit(1);
+	(*closed_list).insert(std::pair<Node*, Node*>(it->first, it->second));
+	if (it->first == target) return PATH_FOUND;
+	(*open_list).erase(it);
+	if ((*open_list).size() == 0) return NO_PATH;
+	aStar(open_list, closed_list, lowest_score, target);
+}
+
+void printPath(Node* origin, Node* target) {
+	if (origin != target)
+		printPath(origin, target->parent);
+	cout << *target;
 }
